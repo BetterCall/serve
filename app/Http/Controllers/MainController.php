dbf094da48604a25651c28aeef1983f0647c011d
BetterCall <bryann@brovia.com>
2017-07-21 10:03:23 +0200

    add firebase-php module

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -2,11 +2,24 @@
 
 namespace App\Http\Controllers;
 
+use App\Providers\FirebaseServiceProvider;
 use Illuminate\Http\Request;
+use Kreait\Firebase
 
 class MainController extends Controller
 {
-    public function facebookReceive( ) {
+    public function facebookReceive(Request $request ) {
+
+        $data = $request->all() ;
+
+        $firebase = app('firebase') ;
+        $firebase->getDatabase()->getReference("/users") ;
+
+
+        // get the current user id
+        $id = $data["entry"] ;
+        dd($id) ;
+        die() ;
 
     }
 }
---
+++ app/Providers/FirebaseServiceProvider.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use Kreait\Firebase\ServiceAccount;
+use Kreait\Firebase\Factory as FirebaseFactory;
+
+class FirebaseServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register the application services.
+     */
+    public function register()
+    {
+        $this->app->singleton(Firebase::class, function () {
+
+            $serviceAccount = ServiceAccount::fromJsonFile(__DIR__."/bump-81ddd-firebase-adminsdk-mb87d-3cf375a0a5.json") ;
+
+            return (new FirebaseFactory())
+                ->withServiceAccount($serviceAccount)
+                ->withDatabaseUri('https://bump-81ddd.firebaseio.com/
+')
+                ->create();
+        });
+
+        $this->app->alias(Firebase::class, 'firebase');
+    }
+}
