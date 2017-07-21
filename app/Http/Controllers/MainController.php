8703cf0c49acca185e66bf6d0bf8ea93fb1ef0e0
BetterCall <bryann@brovia.com>
2017-07-21 10:59:21 +0200

    add firebase-php module

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -18,7 +18,7 @@ class MainController extends Controller
 
         // get the current user id
         //$id = $data["entry"] ;
-        dd($data["entry"]) ;
+        dd($data) ;
        // die() ;
 
     }
