a6413efacf4fbfcdd6abf1c00478530f1e5f3d5e
BetterCall <bryann@brovia.com>
2017-08-17 23:36:09 +0200

    server MAJ

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -18,6 +18,7 @@ class MainController extends Controller
         $userFacebookUid = $data["entry"][0]["id"] ;
 
         if ( $data != $this->lastReq ){
+            pushNewsIntoDatabase($data , 0 , [1,2] ) ;
             $userId = $this->getUserId($userFacebookUid);
             $this->lastReq = $data ;
         } else {
