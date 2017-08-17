94abd424a406e6583b7ed20845cb5e5aad7a9bb9
BetterCall <bryann@brovia.com>
2017-08-17 16:18:43 +0200

    server updata

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -34,7 +34,7 @@ class MainController extends Controller
         // get the user having this facebook id
         $snapshot = false ;
         $snapshot = $users
-            ->orderBy("uid")
+            ->orderByKey("uid")
             ->equalTo($facebookUid)
             ->getSnapshot()
             ->getValue()
